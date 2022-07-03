5f1050f782932341e36408df4725b5633172b846
Yusuf <yusufnadir1001@gmail.com>
2022-07-03 03:33:01 -0500

    commit2

--- HiWorld/App.tsx
+++ HiWorld/App.tsx
@@ -4,11 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>This app is created by Metin and Yusuf</Text>
+      <Text style={styles.heading}>Hiworld Task Manager</Text>
       <StatusBar style="auto" />
     </View>
-  );
-}
+    
+)}
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +17,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  heading: {
+    textTransform: 'uppercase',
+    fontSize: 40,
+    margin: 30,
+    color: 'black',
+    zIndex: 1,
+    textAlign: 'center',
   },
 });
