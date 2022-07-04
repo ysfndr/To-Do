9243424c60a682af3727daa994c6c02be3dcd28e
Yusuf <yusufnadir1001@gmail.com>
2022-07-03 19:21:46 -0500

    commit3

--- HiWorld/App.tsx
+++ HiWorld/App.tsx
@@ -1,14 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View , TextInput , Button } from 'react-native';
+
+interface Props{
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
 
 export default function App() {
+
+  const [todo, setToddo] = useState<string>("");
+
   return (
     <View style={styles.container}>
       <Text>This app is created by Metin and Yusuf</Text>
       <Text style={styles.heading}>Hiworld Task Manager</Text>
       <StatusBar style="auto" />
+      <InputBox todo = {todo} setTodo = {setToddo}/>
     </View>
-    
+)}
+
+const InputBox = ({todo, setTodo}: Props) => {
+  return (
+    <View style={styles.container}>
+      <TextInput
+      value={todo} 
+      onChangeText={ value => setTodo(value) } 
+      placeholder="Enter a task"/>
+      <Button title="GO"/>
+    </View>
 )}
 
 const styles = StyleSheet.create({
