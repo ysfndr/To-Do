e12348e9137d01f4663ed94bc38eeea5082377b0
Yusuf <yusufnadir1001@gmail.com>
2022-07-05 03:44:59 -0500

    commit4

--- HiWorld/App.tsx
+++ HiWorld/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View , TextInput , Button } from 'react-native';
+import { StyleSheet, Text, View , TextInput , Pressable } from 'react-native';
 
 interface Props{
   todo: string;
@@ -13,7 +13,6 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>This app is created by Metin and Yusuf</Text>
       <Text style={styles.heading}>Hiworld Task Manager</Text>
       <StatusBar style="auto" />
       <InputBox todo = {todo} setTodo = {setToddo}/>
@@ -22,12 +21,15 @@ export default function App() {
 
 const InputBox = ({todo, setTodo}: Props) => {
   return (
-    <View style={styles.container}>
+    <View style={styles.input}>
       <TextInput
+      style={styles.input_box}
       value={todo} 
       onChangeText={ value => setTodo(value) } 
       placeholder="Enter a task"/>
-      <Button title="GO"/>
+      <Pressable style={styles.input_submit }>
+        <Text style={styles.input_submit_text}>GO</Text>
+      </Pressable>
     </View>
 )}
 
@@ -36,15 +38,57 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 
   heading: {
     textTransform: 'uppercase',
     fontSize: 25,
-    marginTop: 90,
-    color: 'black',
+    marginTop: 61,
+    color: 'maroon',
     zIndex: 1,
     textAlign: 'center',
   },
+
+  input: {
+    display: 'flex',
+    width: 320,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: 'gray',
+    borderBottomWidth: 1.2,
+    paddingTop: 24,
+    paddingBottom: 5,
+  },
+
+  input_box: {
+    width: 290,
+    borderRadius: 50,
+    paddingRight: 50,
+    paddingVertical: 10,
+    fontSize: 25,
+    position: 'relative',
+  },
+
+  input_submit: {
+    position: 'absolute',
+    width: 45,
+    height: 45,
+    margin: 12,
+    borderRadius: 100,
+    right: 0,
+    fontSize: 15,
+    backgroundColor: 'maroon',
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 3,
+  },
+
+  input_submit_text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+
 });
