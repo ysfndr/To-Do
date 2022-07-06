6b9573f76239d43064259d9975a61fb504bf3a5d
Yusuf <yusufnadir1001@gmail.com>
2022-07-05 19:49:47 -0500

    brokencommit

--- HiWorld/App.tsx
+++ HiWorld/App.tsx
@@ -2,24 +2,45 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View , TextInput , Pressable } from 'react-native';
 
-interface Props{
+interface Props1{
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: () => void;
+}
+
+interface Props2{
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+interface Todo{
+  id: number;
+  todo: string;
+  isDone: boolean;
 }
 
 export default function App() {
 
-  const [todo, setToddo] = useState<string>("");
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]); 
+
+  const handleAdd = () =>{
+    if (todo){
+      setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}]);
+      setTodo("");
+    }
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Hiworld Task Manager</Text>
       <StatusBar style="auto" />
-      <InputBox todo = {todo} setTodo = {setToddo}/>
+      <InputBox todo = {todo} setTodo = {setTodo} handleAdd = {handleAdd}/>
+      <TodoList todos = {todos} setTodos={setTodos}/>
     </View>
 )}
 
-const InputBox = ({todo, setTodo}: Props) => {
+const InputBox = ({todo, setTodo, handleAdd}: Props1) => {
   return (
     <View style={styles.input}>
       <TextInput
@@ -27,11 +48,22 @@ const InputBox = ({todo, setTodo}: Props) => {
       value={todo} 
       onChangeText={ value => setTodo(value) } 
       placeholder="Enter a task"/>
-      <Pressable style={styles.input_submit }>
+      <Pressable style={styles.input_submit } onPress={handleAdd}>
         <Text style={styles.input_submit_text}>GO</Text>
       </Pressable>
     </View>
 )}
+
+const TodoList = ({todos, setTodos}: Props2) => {
+  return (
+    <View style={styles.todos}>
+      Hello World
+      {
+        todos.map((item) => {item.todo})
+      }
+    </View>
+  )
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -89,6 +121,13 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     letterSpacing: 0.25,
     color: 'white',
+  },
+
+  todos: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    width: '90%',
+    flexWrap: 'wrap',
   },
 
 });
